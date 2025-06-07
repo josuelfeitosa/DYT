971feae1520e3f6469d6bb9a40f681139624cf75
josuelfeitosa <josuelfeitosa@gmail.com>
2025-06-07 20:47:35 -0300

    Adiciona Procfile para deploy no Render

--- DYT/settings.py
+++ DYT/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(&rti3w8_mv*0dzkd^erid9s3h)rc#6-c&42zoq^t*i_jf9i@4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DYT.wsgi:application
